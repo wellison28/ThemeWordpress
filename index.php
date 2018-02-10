a0178c32b8441fb8fd4841fce64fc7fe18c93f02
Wellison <wellison28@gmail.com>
2018-02-10 08:01:11 -0200

    create struct basic

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Curso WordPress</title>
+</head>
+<body <?php body_class(); ?>>
+
+	<header>
+		<div class="barra-topo">
+			<div class="redes-sociais"></div>
+			<div class="pesquisa"></div>
+		</div>
+		<div class="area-menu">
+			<div class="logo"></div>
+			<div class="menu"></div>
+		</div>
+	</header>
+	<div class="conteudo">
+		<main>
+			<section class="slide"></section>
+			<section class="servicos"></section>
+			<section class="meio">
+				<aside class="barra-lateral"></aside>
+				<div class="noticias"></div>
+			</section>
+			<section class="mapa"></section>
+		</main>
+	</div>
+	<footer>
+		
+	</footer>
+</body>
+</html>
