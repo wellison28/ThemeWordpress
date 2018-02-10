2be9b255339607353c29d6b0927afd61ce60b509
Wellison <wellison28@gmail.com>
2018-02-10 09:48:42 -0200

    include call for bootstrap

--- functions.php
+++ functions.php
@@ -3,6 +3,15 @@
 function carrega_scripts(){
 
 	wp_enqueue_style('template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all');
+
+	wp_enqueue_style('bootstrap','https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', array(), 
+									'4.0.0', 'all');
+
+	wp_enqueue_script('template', get_template_directory_uri() . '/js/template.js', array(), null, true);
+	// true carrega apenas o script no final do body
+
+	wp_enqueue_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', 
+										array('jquery'), null, true);
 }
 
 add_action('wp_enqueue_scripts', 'carrega_scripts');
